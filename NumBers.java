080cb58f7db9d6ea3f0549fad4a53fe1b588cc90
Rasel <harunorrashid289@gmail.com>
2026-06-29 12:39:49 +0600

    Numbers Pattern

---
+++ NumBers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class NumBers {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        IO.print("ENter a NumBer: ");
+        int n=input.nextInt();
+        int row,col;
+        for(row=n;row>0;row--)
+        {
+            for(col=0;col<n;col++)
+            {
+                IO.print(row);
+            }
+            IO.println();
+        }
+
+    }
+    
+}
