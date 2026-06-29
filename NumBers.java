6dc3ed6a2c416ee98298ebd62ad27d15dfb25c7b
Rasel <harunorrashid289@gmail.com>
2026-06-29 13:49:15 +0600

    add 2.6. Hollow Square of Numbers

--- NumBers.java
+++ NumBers.java
@@ -5,12 +5,14 @@ public class NumBers {
         IO.print("ENter a NumBer: ");
         int n=input.nextInt();
         int row,col,c=0;
-        for(row=n;row>0;row--)
+        for(row=0;row<n;row++)
         {
             for(col=0;col<n;col++)
             {
-                c++;
-                IO.print((c)%2+" ");
+                if(row==0||row==n-1||col==0||col==n-1)
+                    IO.print(c+1);
+                else IO.print(0);
+
                 
             }
             IO.println();
